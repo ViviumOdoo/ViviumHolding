52cec63ce387cdb8c181d294465c2bc426dde97c
Pankaj <114057234+awbpankaj@users.noreply.github.com>
2022-11-17 01:51:27 +0530

    [Add: created new fabric color menus and views for import data]

--- vvm_model/__manifest__.py
+++ vvm_model/__manifest__.py
@@ -20,6 +20,7 @@
 	'data': [
 		'security/ir.model.access.csv',
 		'data/product_storable_cron.xml',
+		'views/fabric_color_view.xml',
 		'views/menu_model_view.xml',
 		'views/vvm_model_view.xml',
 		'views/model_fabric.xml',
@@ -28,7 +29,7 @@
 		'views/sale_order_view.xml',
 		'views/stock_picking_view.xml',
 		'views/res_user_view.xml',
-		'views/fabric_color_view.xml'
+
 	],
 	'installable': True,
 	'application': True,
